06a5b8eb1504813b34ef6597639ceb62cfbb4432
MiguelCastroSM <nhisper@gmail.com>
2021-05-09 18:41:07 -0300

    creacion

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	export let name;
+	import CustomButton from './CustomButton.svelte';
+
+function handleClick() {
+	alert('clicked');
+}
+</script>
+
+<main>
+	<h1> Stone Control Stock</h1>
+	
+	<CustomButton nombre="Controlar Stock" on:click={handleClick}/>
+	<CustomButton nombre="Agregar Stock" on:click={handleClick}/>
+	<CustomButton nombre="Quitar  &nbsp;Stock &nbsp;" on:click={handleClick}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		background-color: whitesmoke;
+	}
+
+	h1 {
+		color: #44b6ac;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/CustomButton.svelte
@@ -0,0 +1,27 @@
+<button on:click>
+	{nombre}
+</button>
+
+<script>
+	// your script goes here
+	export let nombre={nombre};
+</script>
+
+<style>
+	button {
+		height: 4rem;
+		width: 8rem;
+		background-color:#44b6ac;
+		border-color: #555;
+		color: #555;
+		font-size: 1.25rem;
+		background-image: linear-gradient(45deg, #555 50%, transparent 50%);
+		background-position: 100%;
+		background-size: 400%;
+		transition: background 300ms ease-in-out;
+	}
+	button:hover {
+		background-position: 0;
+		color: #44b6ac;
+	}
+</style>
